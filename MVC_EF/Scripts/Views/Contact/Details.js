36921bf3840b6f300034e2efdfced6506ef7e2df
Armando Bustos <armanjbustos596@gmail.com>
2020-07-04 18:02:23 -0600

    calculate age

---
+++ MVC_EF/Scripts/Views/Contact/Details.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    const birthdate = $('#contactAge').html().split('/');
+
+    $('#contactAge').html(calculateAge(...birthdate));
+});
+
+function calculateAge(year, month, day) {
+    const diff = Date.now() - new Date(year, month - 1, day);
+
+    return Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+}
